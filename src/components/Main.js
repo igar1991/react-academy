df19388cad6b2d5b74656e2beaf7bc370001c7f1
igar1991 <iiggaarr19912004@gmail.com>
2018-12-16 18:38:00 +0300

    Все сделал

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 import Row from 'react-bootstrap/lib/Row';
 import Col from 'react-bootstrap/lib/Col';
 import styled from 'styled-components';
+import Slader from './Slider.js'
+
 
 const Repair = styled.div`
 color: #ffffff;
@@ -49,6 +51,7 @@ class Main extends React.Component {
 
 			  </Col>
 			  <Col lg={6} lgOffset={1}>
+			  <Slader />
 
 			  </Col>
 
